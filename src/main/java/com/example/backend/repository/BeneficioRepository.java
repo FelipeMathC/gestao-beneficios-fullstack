c8429a13065d97276f757da6da8993cf377c9aa8
FelipeMathC <felipecalldeira@gmail.com>
2026-04-14 11:51:53 -0300

    Delete src directory

--- src/main/java/com/example/backend/repository/BeneficioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.model.Beneficio;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BeneficioRepository extends JpaRepository<Beneficio, Long> {
-}
